6286a6e87d847e9e03f4241de9ef9eb8d5c11512
MaTrIxGg <jsgg01xd@gmail.com>
2018-11-29 02:20:55 -0500

    Correciones menores

--- Menu_principal.php
+++ Menu_principal.php
@@ -129,7 +129,7 @@
 			<div class="row">
 				<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-5">
 					<ul class="list-unstyled list-inline social text-center">
-						<li class="list-inline-item"><a href="https://www.facebook.com/si.farmlands/"><i class="fa fa-facebook"></i></a></li>
+						<li class="list-inline-item"><a href="https://www.facebook.com/si.farmlands/" target="_blank"><i class="fa fa-facebook"></i></a></li>
 						<li class="list-inline-item"><a href="javascript:void();"><i class="fa fa-twitter"></i></a></li>
 						<li class="list-inline-item"><a href="javascript:void();"><i class="fa fa-instagram"></i></a></li>
 						<li class="list-inline-item"><a href="javascript:void();"><i class="fa fa-google-plus"></i></a></li>
