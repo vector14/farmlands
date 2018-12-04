3d19a074286eef1cc1dbe5a6dac79f6c12201465
MaTrIxGg <jsgg01xd@gmail.com>
2018-12-04 00:08:02 -0500

    actualizacion de repositorio

--- conexion.php
+++ conexion.php
@@ -1,9 +1,9 @@
 <?php
 function conectar(){
-$localhost="localhost";
-  $usuario="root";
-  $contraseña="";
-  $basededatos="farmlandsdb";
+        $localhost="den1.mysql5.gear.host";
+        $usuario="farmlandsdb";
+        $contraseña="Gf7lq?P5~5oH";
+        $basededatos="farmlandsdb";
             $mysqli = new mysqli("$localhost", "$usuario", "$contraseña" , "$basededatos");	
             if ($mysqli->connect_errno) {
               echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
